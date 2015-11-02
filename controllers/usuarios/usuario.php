f93e807ea934c5671ddae5c0da16163611c7ea9c
Ardrein <hernandaresu@gmail.com>
2015-11-02 14:40:47 -0500

    Update and rename controllers/usuairos/usuario.php to controllers/usuarios/usuario.php

--- controllers/usuarios/usuario.php
+++ controllers/usuarios/usuario.php
@@ -1,27 +1,23 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Usuario extends CI_Controller{
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('form');
 		$this->load->model('usuarios/usuario_model');
 	}
-
 	function index(){
-
 		$this->load->view('usuario/header');
 	}
-
 	function nuevo(){
 		$this->load->view('usuario/header');
 		$this->load->view('usuario/formularioUsuario');
 	}
-
 	function recibirdatos(){
 		$data = array(
 			'nombre' => $this->input->post('nombre'),
 			'cedula' => $this->input->post('cedula'),
 			'licencia' => $this->input->post('licencia'),
+			'email' => $this->input->post('email'),
 			'telefono' => $this->input->post('telefono'),
 			'direccion' => $this->input->post('direccion')
 			);
@@ -30,7 +26,5 @@ class Usuario extends CI_Controller{
 		$this->load->view('usuario/header');
 		$this->load->view('usuario/formularioUsuario');
 	}
-
 }
-
 ?>
