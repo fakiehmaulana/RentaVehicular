6fb567aa5cc304f6758ca49c6453dafa9eb58200
Ardrein <hernandaresu@gmail.com>
2015-11-02 14:40:06 -0500

    Update usuario_model.php

--- models/usuarios/usuario_model.php
+++ models/usuarios/usuario_model.php
@@ -1,18 +1,20 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class usuario_model	 extends CI_Model{
+class Usuario_model	 extends CI_Model{
 	function __construct(){
 		parent:: __construct();
 		$this->load->database();
 	}
 
 	function crearUsuario($data){
-		$datos = array('nombre' => $data['nombre'], 'cedula' => $data['cedula'], 'licencia' => $data['licencia'],
+		$datos = array(
+			'nombre' => $data['nombre'], 
+			'cedula' => $data['cedula'], 
+			'licenciaConduccion' => $data['licencia'],
+			'email' => $data['email'],
 			'telefono' => $data['telefono'],
 			'direccion' => 	$data['direccion']
 			);
-
 		$this->db->insert('usuario' , $datos);
 	}
 }
-
 ?>
