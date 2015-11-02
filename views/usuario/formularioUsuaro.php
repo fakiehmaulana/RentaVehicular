397b8ab6f4319ac39099d28f9e72e618caa8073d
Ardrein <hernandaresu@gmail.com>
2015-11-02 14:39:46 -0500

    Update formularioUsuaro.php

--- views/usuario/formularioUsuaro.php
+++ views/usuario/formularioUsuaro.php
@@ -1,21 +1,17 @@
 <?= form_open("/usuarios/usuario/recibirdatos") ?>
 <?php
-
 	$nombre = array(
 		'name' => 'nombre' ,
 		'placeholder' => 'Escribe tu nombre'
 	);
-
 	$cedula = array(
 		'name' => 'cedula' ,
 		'placeholder' => 'Numero de cedula'
 	);
-
 	$licencia = array(
 		'name' => 'licencia' ,
 		'placeholder' => 'Numero de licencia'
 	);
-
 	$telefono = array(
 		'name' => 'telefono' ,
 		'placeholder' => 'Escribe tu telefono'
@@ -23,6 +19,10 @@
 	$direccion = array(
 		'name' => 'direccion' ,
 		'placeholder' => 'Escribe tu direccion'
+	);
+	$email = array(
+		'name' => 'email' ,
+		'placeholder' => 'Escribe tu correo'
 	);
 ?>
 	<?= form_label('Nombre:' , 'nombre') ?>
@@ -35,6 +35,10 @@
 
 	<?= form_label('Licencia:' , 'licencia') ?>
 	<?= form_input($licencia) ?>
+	<br>
+
+	<?= form_label('Email:' , 'email') ?>
+	<?= form_input($email) ?>
 	<br>
 
 	<?= form_label('Telefono:' , 'telefono') ?>
