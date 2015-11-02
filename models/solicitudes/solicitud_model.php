e05dca0ef12b435bc29754dbb2f8423d4e2cbacc
Ardrein <hernandaresu@gmail.com>
2015-11-01 23:19:23 -0500

    Create solicitud_model.php
    Version 1 del modelo de las solicitudes de renta.

---
+++ models/solicitudes/solicitud_model.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Solicitud_model	 extends CI_Model{
+	function __construct(){
+		parent::__construct();
+		$this->load->database();
+	}
+
+	/*
+	*Funcion para insertar en la base de datos.
+	*/
+	function crearSolicitud($data){
+		$datos = array(
+			'Fecha' => 			$data['fecha'],
+			'Hora' => 			$data['hora'],
+			'Kilometraje' => 	$data['kilometraje'],
+			'idVehiculo' => 	$data['vehiculo'],
+			'idUsuario' => 		$data['usuario'],
+		 );
+
+		$this->db->insert('solicitudrenta',$datos);
+
+	}
+
+	/*
+	*Funcion para cargar todas las solicitudes de renta de la base de datos.
+	*/
+	function cargarSolicitudes(){
+		$query = $this->db->get('solicitudrenta');
+		if($query->num_rows > 0) return $query;
+		else return false;
+	}
+
+
+	/*
+	*Funcion para cargar todas una solicitud de renta de la base de datos.
+	*/
+	function cargarSolicitud($id){
+		$this->db->where('idSolicitudRenta',$id);
+		$query = $this->db->get('solicitudrenta');
+		if($query->num_rows > 0) return $query;
+		else return false;
+	}
+}
+
+?>
